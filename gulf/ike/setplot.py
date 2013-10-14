9a049cce23ebcc37381ef2eda7b5c3ed6a25921a
Kyle Mandli <kyle.mandli@gmail.com>
2013-10-14 15:20:21 -0500

    Add article plotting option

--- gulf/ike/setplot.py
+++ gulf/ike/setplot.py
@@ -7,6 +7,8 @@ function setplot is called to set the plot parameters.
     
 """
 
+article = True
+
 import os
 
 import numpy
@@ -14,6 +16,9 @@ import scipy.io
 
 # Plot customization
 import matplotlib
+
+# Use LaTeX for all text
+matplotlib.rcParams['text.usetex'] = True
 
 # Markers and line widths
 matplotlib.rcParams['lines.linewidth'] = 2.0
@@ -28,8 +33,10 @@ matplotlib.rcParams['xtick.labelsize'] = 16
 matplotlib.rcParams['ytick.labelsize'] = 16
 
 # DPI of output images
-# matplotlib.rcParams['savefig.dpi'] = 300
-matplotlib.rcParams['savefig.dpi'] = 100
+if article:
+    matplotlib.rcParams['savefig.dpi'] = 300
+else:
+    matplotlib.rcParams['savefig.dpi'] = 100
 
 import matplotlib.pyplot as plt
 import datetime
@@ -237,7 +244,10 @@ def setplot(plotdata):
                                                shrink=gulf_shrink)
     surge.plot.add_land(plotaxes,topo_min=-10.0,topo_max=5.0)
     # surge.plot.add_bathy_contours(plotaxes)
-    add_custom_colorbar_ticks_to_axes(plotaxes, 'surface', surface_ticks, surface_labels)
+    if article:
+        plotaxes.plotitem_dict['surface'].add_colorbar = False
+    else:
+        add_custom_colorbar_ticks_to_axes(plotaxes, 'surface', surface_ticks, surface_labels)
     plotaxes.plotitem_dict['surface'].amr_patchedges_show = [1,1,1,1,1,1,1,1]
 
     #
@@ -259,7 +269,10 @@ def setplot(plotdata):
     surge.plot.add_speed(plotaxes, plot_type='contourf', 
                                    contours=speed_contours, 
                                    shrink=gulf_shrink)
-    add_custom_colorbar_ticks_to_axes(plotaxes, 'speed', speed_ticks, speed_labels)
+    if article:
+        plotaxes.plotitem_dict['speed'].add_colorbar = False
+    else:
+        add_custom_colorbar_ticks_to_axes(plotaxes, 'speed', speed_ticks, speed_labels)
 
     # Land
     surge.plot.add_land(plotaxes)
@@ -296,15 +309,22 @@ def setplot(plotdata):
     latex_ylimits = [27.5,30.5]
     latex_shrink = 1.0
     def latex_after_axes(cd):
-        plt.subplots_adjust(right=1.0)
+        if article:
+            plt.subplots_adjust(left=0.07, bottom=0.14, right=1.0, top=0.86)
+        else:
+            plt.subplots_adjust(right=1.0)
         surge_afteraxes(cd)
+
     #
     # Surface
     #
     plotfigure = plotdata.new_plotfigure(name='Surface - LaTex Shelf', 
                                          figno=fig_num_counter.get_counter())
     plotfigure.show = True
-    plotfigure.kwargs = {'figsize':(9,2.7), 'facecolor':'none'}
+    if article:
+        plotfigure.kwargs = {'figsize':(8,2.7), 'facecolor':'none'}
+    else:
+        plotfigure.kwargs = {'figsize':(9,2.7), 'facecolor':'none'}
 
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
@@ -317,13 +337,20 @@ def setplot(plotdata):
     surge.plot.add_surface_elevation(plotaxes, plot_type='contourf', 
                                                contours=surface_contours,
                                                shrink=latex_shrink)
-    add_custom_colorbar_ticks_to_axes(plotaxes, 'surface', [-5,-2.5,0,2.5,5.0], 
+
+    if article:
+        plotaxes.plotitem_dict['surface'].add_colorbar = False
+        # plotaxes.afteraxes = lambda cd: article_latex_after_axes(cd, landfall)
+    else:
+        add_custom_colorbar_ticks_to_axes(plotaxes, 'surface', [-5,-2.5,0,2.5,5.0], 
                                     ["-5.0","-2.5"," 0"," 2.5"," 5.0"])
     # plotaxes.plotitem_dict['surface'].contour_cmap = plt.get_cmap('OrRd')
     # surge.plot.add_surface_elevation(plotaxes,plot_type='contour')
     surge.plot.add_land(plotaxes)
-    plotaxes.plotitem_dict['surface'].amr_patchedges_show = [1,1,1,0,0,0,0]
-    plotaxes.plotitem_dict['land'].amr_patchedges_show = [1,1,1,0,0,0,0]
+    # plotaxes.plotitem_dict['surface'].amr_patchedges_show = [1,1,1,0,0,0,0]
+    plotaxes.plotitem_dict['surface'].amr_patchedges_show = [0,0,0,0,0,0,0]
+    # plotaxes.plotitem_dict['land'].amr_patchedges_show = [1,1,1,0,0,0,0]
+    plotaxes.plotitem_dict['land'].amr_patchedges_show = [0,0,0,0,0,0,0]
 
     # Plot using jet and 0.0 to 5.0 to match figgen generated ADCIRC results
     # plotaxes.plotitem_dict['surface'].pcolor_cmin = 0.0
@@ -336,7 +363,10 @@ def setplot(plotdata):
     plotfigure = plotdata.new_plotfigure(name='Currents - LaTex Shelf',  
                                          figno=fig_num_counter.get_counter())
     plotfigure.show = True
-    plotfigure.kwargs = {'figsize':(9,2.7), 'facecolor':'none'}
+    if article:
+        plotfigure.kwargs = {'figsize':(8,2.7), 'facecolor':'none'}
+    else:
+        plotfigure.kwargs = {'figsize':(9,2.7), 'facecolor':'none'}
 
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
@@ -349,11 +379,18 @@ def setplot(plotdata):
     surge.plot.add_speed(plotaxes, plot_type='contourf', 
                                    contours=speed_contours, 
                                    shrink=latex_shrink)
-    add_custom_colorbar_ticks_to_axes(plotaxes, 'speed', speed_ticks, speed_labels)
+
+    if article:
+        plotaxes.plotitem_dict['speed'].add_colorbar = False
+    else:
+        add_custom_colorbar_ticks_to_axes(plotaxes, 'speed', speed_ticks, speed_labels)
     # surge.plot.add_surface_elevation(plotaxes,plot_type='contour')
     surge.plot.add_land(plotaxes)
-    plotaxes.plotitem_dict['speed'].amr_patchedges_show = [1,1,0,0,0,0,0]
-    plotaxes.plotitem_dict['land'].amr_patchedges_show = [1,1,1,0,0,0,0]
+    # plotaxes.plotitem_dict['speed'].amr_patchedges_show = [1,1,0,0,0,0,0]
+    # plotaxes.plotitem_dict['land'].amr_patchedges_show = [1,1,1,0,0,0,0]
+    plotaxes.plotitem_dict['speed'].amr_patchedges_show = [0,0,0,0,0,0,0]
+    plotaxes.plotitem_dict['land'].amr_patchedges_show = [0,0,0,0,0,0,0]
+
 
 
     # ========================================================================
@@ -363,7 +400,10 @@ def setplot(plotdata):
     houston_ylimits = [29.1, 29.0 + 55.0 / 60.0]
     houston_shrink = 0.9
     def houston_after_axes(cd):
-        plt.subplots_adjust(left=0.12, bottom=0.06, right=0.97, top=0.97)
+        if article:
+            plt.subplots_adjust(left=0.05, bottom=0.07, right=0.99, top=0.92)
+        else:
+            plt.subplots_adjust(left=0.12, bottom=0.06, right=0.97, top=0.97)
         surge_afteraxes(cd)
         # surge.plot.gauge_locations(cd)
     
@@ -373,6 +413,8 @@ def setplot(plotdata):
     plotfigure = plotdata.new_plotfigure(name='Surface - Houston/Galveston',  
                                          figno=fig_num_counter.get_counter())
     plotfigure.show = True
+    # if article:
+    #     plotfigure.kwargs['figsize'] = 
 
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
@@ -385,8 +427,14 @@ def setplot(plotdata):
     surge.plot.add_surface_elevation(plotaxes, plot_type='contourf', 
                                                contours=surface_contours,
                                                shrink=houston_shrink)
-    add_custom_colorbar_ticks_to_axes(plotaxes, 'surface', surface_ticks, surface_labels)
+    
+    if article:
+        plotaxes.plotitem_dict['surface'].add_colorbar = False
+    else:
+        add_custom_colorbar_ticks_to_axes(plotaxes, 'surface', surface_ticks, surface_labels)
     surge.plot.add_land(plotaxes)
+    plotaxes.plotitem_dict['surface'].amr_patchedges_show = [0,0,0,0,0,0,0]
+    plotaxes.plotitem_dict['land'].amr_patchedges_show = [0,0,0,0,0,0,0]
     # surge.plot.add_bathy_contours(plotaxes)
 
     # Plot using jet and 0.0 to 5.0 to match figgen generated ADCIRC results
@@ -412,11 +460,17 @@ def setplot(plotdata):
     surge.plot.add_speed(plotaxes, plot_type='contourf', 
                                    contours=speed_contours,
                                    shrink=houston_shrink)
-    add_custom_colorbar_ticks_to_axes(plotaxes, 'speed', speed_ticks, speed_labels)
+    
+    if article:
+        plotaxes.plotitem_dict['speed'].add_colorbar = False
+    else:
+        add_custom_colorbar_ticks_to_axes(plotaxes, 'speed', speed_ticks, speed_labels)
     surge.plot.add_land(plotaxes)
     # surge.plot.add_bathy_contours(plotaxes)
     # plotaxes.plotitem_dict['speed'].amr_patchedges_show = [1,1,1,1,1,1,1,1]
     # plotaxes.plotitem_dict['land'].amr_patchedges_show = [1,1,1,1,1,1,1,1]
+    plotaxes.plotitem_dict['speed'].amr_patchedges_show = [0,0,0,0,0,0,0]
+    plotaxes.plotitem_dict['land'].amr_patchedges_show = [0,0,0,0,0,0,0]
 
     # ==========================
     #  Hurricane Forcing fields
@@ -460,7 +514,7 @@ def setplot(plotdata):
                     type='each_gauge')
     plotfigure.show = True
     plotfigure.clf_each_gauge = True
-    plotfigure.kwargs['figsize'] = (16,10)
+    # plotfigure.kwargs['figsize'] = (16,10)
 
     def gauge_after_axes(cd):
 
@@ -484,13 +538,13 @@ def setplot(plotdata):
                      ADCIRC_gauge[:,1] + gauge_surface_offset[1], 'r-.', label="ADCIRC")
 
             # Fix up plot
-            axes.set_title('Station %s' % gauge_name_trans[cd.gaugeno])
+            axes.set_title('Station %s' % cd.gaugeno)
             axes.set_xlabel('Days relative to landfall')
             axes.set_ylabel('Surface (m)')
             axes.set_xlim([-2,1])
             axes.set_ylim([-1,5])
             axes.set_xticks([-2,-1,0,1])
-            axes.set_xticklabels([-2,-1,0,1])
+            axes.set_xticklabels([r"$-2$",r"$-1$",r"$0$",r"$1$"])
             axes.grid(True)
             axes.legend()
 
@@ -520,7 +574,7 @@ def setplot(plotdata):
     gauge_ylimits = [29.0, 30.0]
     gauge_location_shrink = 0.75
     def gauge_after_axes(cd):
-        # plt.subplots_adjust(left=0.12, bottom=0.06, right=0.97, top=0.97)
+        plt.subplots_adjust(left=0.12, bottom=0.06, right=0.97, top=0.97)
         surge_afteraxes(cd)
         surge.plot.gauge_locations(cd, gaugenos=[1, 2, 3, 4])
         plt.title("Gauge Locations")
@@ -680,21 +734,35 @@ def setplot(plotdata):
     surge.plot.add_land(plotaxes)
 
     #-----------------------------------------
-    
+
     # Parameters used only when creating html and/or latex hardcopy
     # e.g., via pyclaw.plotters.frametools.printframes:
 
-    plotdata.printfigs = True                # print figures
-    plotdata.print_format = 'png'            # file format
-    plotdata.print_framenos = 'all'            # list of frames to print
-    plotdata.print_gaugenos = [1,2,3,4]          # list of gauges to print
-    plotdata.print_fignos = 'all'            # list of figures to print
-    plotdata.html = True                     # create html files of plots?
-    plotdata.html_homelink = '../README.html'   # pointer for top of index
-    plotdata.latex = True                    # create latex file of plots?
-    plotdata.latex_figsperline = 2           # layout of plots
-    plotdata.latex_framesperline = 1         # layout of plots
-    plotdata.latex_makepdf = False           # also run pdflatex?
+    if article:
+        plotdata.printfigs = True                # print figures
+        plotdata.print_format = 'png'            # file format
+        plotdata.print_framenos = [54,60,66,72,78,84]            # list of frames to print
+        plotdata.print_gaugenos = [1,2,3,4]          # list of gauges to print
+        plotdata.print_fignos = [4,5,6,7,10,3,300]            # list of figures to print
+        plotdata.html = True                     # create html files of plots?
+        plotdata.html_homelink = '../README.html'   # pointer for top of index
+        plotdata.latex = False                    # create latex file of plots?
+        plotdata.latex_figsperline = 2           # layout of plots
+        plotdata.latex_framesperline = 1         # layout of plots
+        plotdata.latex_makepdf = False           # also run pdflatex?
+
+    else:
+        plotdata.printfigs = True                # print figures
+        plotdata.print_format = 'png'            # file format
+        plotdata.print_framenos = 'all'            # list of frames to print
+        plotdata.print_gaugenos = [1,2,3,4]          # list of gauges to print
+        plotdata.print_fignos = 'all'            # list of figures to print
+        plotdata.html = True                     # create html files of plots?
+        plotdata.html_homelink = '../README.html'   # pointer for top of index
+        plotdata.latex = True                    # create latex file of plots?
+        plotdata.latex_figsperline = 2           # layout of plots
+        plotdata.latex_framesperline = 1         # layout of plots
+        plotdata.latex_makepdf = False           # also run pdflatex?
 
     return plotdata
 
