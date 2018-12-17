79e390e9cedea769d1bc4a8d3c21a58f20634a92
cr2940 <36740525+cr2940@users.noreply.github.com>
2018-12-16 21:45:19 -0500

    Update thredds.py
    indentation correction

--- atlantic/bathy/thredds.py
+++ atlantic/bathy/thredds.py
@@ -12,42 +12,41 @@ server_url = 'https://www.ngdc.noaa.gov/thredds/'
 # Everything after "catalog/"
 request_url = 'catalog/tiles/tiled_19as/'
 
-   
 def get_elements(url, tag_name, attribute_name):
-  """Get elements from an XML file"""
-  # usock = urllib2.urlopen(url)
-  usock = urlopen(url)
-  xmldoc = minidom.parse(usock)
-  usock.close()
-  tags = xmldoc.getElementsByTagName(tag_name)
-  attributes=[]
-  for tag in tags:
-    attribute = tag.getAttribute(attribute_name)
-    attributes.append(attribute)
-  return attributes
+    """Get elements from an XML file"""
+    # usock = urllib2.urlopen(url)
+    usock = urlopen(url)
+    xmldoc = minidom.parse(usock)
+    usock.close()
+    tags = xmldoc.getElementsByTagName(tag_name)
+    attributes=[]
+    for tag in tags:
+        attribute = tag.getAttribute(attribute_name)
+        attributes.append(attribute)
+    return attributes
  
 def main():
-  url = server_url + request_url + 'catalog.xml'
-  print(url)
-  catalog = get_elements(url,'dataset','urlPath')
-  files=[]
-  for citem in catalog:
+    url = server_url + request_url + 'catalog.xml'
+    print(url)
+    catalog = get_elements(url,'dataset','urlPath')
+    files=[]
+    for citem in catalog:
     
-    if (citem[:27]=='tiles/tiled_19as/ncei19_n40') or (citem[:27]=='tiles/tiled_19as/ncei19_n41') and (citem[-3:]=='.nc'):
-      files.append(citem)
-  count = 0
-  for f in files:
-    #if f== 'tiles/tiled_19as/ncei19_n40x75_w074x00_2015v1.nc' or f =='tiles/tiled_19as/ncei19_n40x75_w074x25_2015v1.nc' or f=='tiles/tiled_19as/ncei19_n40x75_w073x75_2015v1.nc':
-     #   continue
-    count +=1
-    file_url = server_url + 'fileServer/' + f
-    file_prefix = file_url.split('/')[-1][:-3]
-    file_name = file_prefix + '.nc'
-    print('Downloading file %d of %d' % (count,len(files)))
-    print(file_name)
-    a = urlretrieve(file_url,file_name)
-    print(a)
+        if (citem[:27]=='tiles/tiled_19as/ncei19_n40') or (citem[:27]=='tiles/tiled_19as/ncei19_n41') and (citem[-3:]=='.nc'):
+            files.append(citem)
+    count = 0
+    for f in files:
+        #if f== 'tiles/tiled_19as/ncei19_n40x75_w074x00_2015v1.nc' or f =='tiles/tiled_19as/ncei19_n40x75_w074x25_2015v1.nc' or f=='tiles/tiled_19as/ncei19_n40x75_w073x75_2015v1.nc':
+         #   continue
+        count +=1
+        file_url = server_url + 'fileServer/' + f
+        file_prefix = file_url.split('/')[-1][:-3]
+        file_name = file_prefix + '.nc'
+        print('Downloading file %d of %d' % (count,len(files)))
+        print(file_name)
+        a = urlretrieve(file_url,file_name)
+        print(a)
  
 # Run main function when in comand line mode        
 if __name__ == '__main__':
-  main()
+    main()
