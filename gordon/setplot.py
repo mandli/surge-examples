f613a28a09738295b78f53baec6867e68f07dc80
Xinyi0000 <111918557+Xinyi0000@users.noreply.github.com>
2022-08-28 14:20:07 -0400

    Update setplot.py

--- gordon/setplot.py
+++ gordon/setplot.py
@@ -4,9 +4,8 @@ from __future__ import print_function
 
 import os
 
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-import pandas
 import datetime
 
 import clawpack.visclaw.colormaps as colormap
@@ -14,9 +13,12 @@ import clawpack.visclaw.gaugetools as gaugetools
 import clawpack.clawutil.data as clawutil
 import clawpack.amrclaw.data as amrclaw
 import clawpack.geoclaw.data as geodata
+
 import clawpack.geoclaw.util
 
 import clawpack.geoclaw.surge.plot as surgeplot
+import csv
+from clawpack.geoclaw.util import fetch_noaa_tide_data
 
 try:
     from setplotfg import setplotfg
@@ -54,8 +56,7 @@ def setplot(plotdata=None):
                                              kwargs={"markersize": 4})
 
     # Color limits
-    # --> change?
-    surface_limits = [-1.0, 1.0]
+    surface_limits = [-2.0, 2.0]
     speed_limits = [0.0, 3.0]
     wind_limits = [0, 64]
     pressure_limits = [935, 1013]
@@ -70,12 +71,9 @@ def setplot(plotdata=None):
     regions = {"Gulf": {"xlimits": (clawdata.lower[0], clawdata.upper[0]),
                         "ylimits": (clawdata.lower[1], clawdata.upper[1]),
                         "figsize": (6.4, 4.8)},
-               "LaTex Shelf": {"xlimits": (-92, -86), #change (-92, -86)
-                               "ylimits": (29, 32), #change (29, 32)
-                               "figsize": (8, 2.7)}, # Expand domain of LaTex shelf: wider and larger
-               "Mobile Bay": {"xlimits": (-89, -87), #change (-88.27, -88.03)
-                               "ylimits": (30.0, 31.0), #change (30.23, 30.79)
-                               "figsize": (4, 2)}}
+               "LaTex Shelf": {"xlimits": (-89, -87), #change (-92, -86)
+                               "ylimits": (30, 31), #change (29, 32)
+                               "figsize": (4, 2)}} # Expand domain of LaTex shelf: wider and larger
 
     for (name, region_dict) in regions.items():
 
@@ -160,58 +158,71 @@ def setplot(plotdata=None):
     plotfigure.show = True
     plotfigure.clf_each_gauge = True
 
+    stations = [('8741533', 'Pascagoula NOAA Lab, MS'),
+                ('8735180', 'Dauphin Island, AL'),
+                ('8736897','Coast Guard Sector Mobile, AL'),
+                ('8729840', 'Pensacola, FL')]
+
+    landfall_time = np.datetime64('2018-09-05T12:00')
+    begin_date = datetime.datetime(2018, 9, 3, 12, 0)
+    end_date = datetime.datetime(2018, 9, 7, 12, 0)
+
+    def get_actual_water_levels(station_id):
+        # Fetch water levels and tide predictions for given station
+        date_time, water_level, tide = fetch_noaa_tide_data(station_id,
+                begin_date, end_date)
+
+        # Calculate times relative to landfall
+        days_rel_landfall = (date_time - landfall_time) / np.timedelta64(1, 's') /86400
+        
+        # Subtract tide predictions from measured water levels
+        water_level -= tide
+
+        # Find the mean values
+        # Data imported every 6 minutes (i.e. 360 seconds)
+        num_data_pts = (end_date - begin_date).total_seconds() / 360
+        mean_value = np.sum(water_level) / num_data_pts
+        water_level -= mean_value
+
+        return days_rel_landfall, water_level
+
+
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.xlimits = [-2, 2] #change
+    plotaxes.xlimits = [-2, 2]
     # plotaxes.xlabel = "Days from landfall"
     # plotaxes.ylabel = "Surface (m)"
-    plotaxes.ylimits = [-0.5, 1] #change
+    plotaxes.ylimits = [-1, 1.5]
     plotaxes.title = 'Surface'
 
-    import datetime
-
-    #my_dict = {1: '8739803', 2: '8738043', 3: '8735523', 4: '8735391', 5: '8735180',
-    #           6: '8737138', 7: '8736897', 8: '8737048', 9: '8732828', 10: '8729840'}
-    
-    my_dict = {1: '8741533', 2: '8735180', 3: '8736897', 4: '8737048', 5: '8729840'}
-   
-    offsetwater = [0.32, 0.31, 0.248, 0.22, 0.24]
-    
-    def gauge_afteraxes(cd): # TO DO: how to plot, what is cd, where is data being read in?
-        t0 = datetime.datetime(2018, 9, 3, 12)
-        t_offset = datetime.datetime(2018, 9, 5, 12)
-        tf = datetime.datetime(2018, 9, 7, 12)
-        date_time, water_level, prediction = clawpack.geoclaw.util.fetch_noaa_tide_data(my_dict[cd.gaugeno], t0, tf)
-
+    def gauge_afteraxes(cd):
+        station_id, station_name = stations[cd.gaugeno-1]
+        secs_rel_landfall, actual_level = get_actual_water_levels(station_id)
+        
         axes = plt.gca()
+        
         surgeplot.plot_landfall_gauge(cd.gaugesoln, axes)
 
-        date_time = date_time.astype('O')
-        t = numpy.empty(date_time.shape[0])
-        for j, dt in enumerate(date_time):
-            t[j] = (dt - t_offset).total_seconds() / 86400
-        #axes.plot(t, water_level - prediction)
-        axes.plot(t, water_level - prediction - offsetwater[cd.gaugeno-1])
-       
+        axes.plot(secs_rel_landfall, actual_level, 'c')
+
+    
         # Fix up plot - in particular fix time labels
-        axes.set_title('Station %s' % cd.gaugeno)
+        axes.set_title(station_name )
         axes.set_xlabel('Days relative to landfall')
         axes.set_ylabel('Surface (m)')
-        axes.set_xlim([-2, 2]) # change to t_0 and t_1
-        axes.set_ylim([-0.5, 3])
-        axes.set_xticks([-2.0, -1.5, -1.0, -0.5, 0, 0.5, 1, 1.5, 2]) # adjust x_ticks appropriately
-        axes.set_xticklabels([r"$-2.0$", r"$-1.5$", r"$-1.0$", r"$-0.5$", r"$-0$", r"$0.5$", r"$1.0$", r"$1.5$", r"$2.0$"])
-        axes.set_yticks([-.5, 0, .5, 1])
-        axes.set_yticklabels([r"$-.5$", r"$0$", r"$.5$", r"$1$"])
+        axes.set_xlim([-2, 2])
+        axes.set_ylim([-1, 1.5])
+        axes.set_xticks(np.arange(-2,2.5,0.5))
+        axes.set_yticks(np.arange(-1,2,0.5))
         axes.grid(True)
-     
+        
+    plotaxes = plotfigure.new_plotaxes()
     plotaxes.afteraxes = gauge_afteraxes
-    
     # Plot surface as blue curve:
     plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
     plotitem.plot_var = 0
     plotitem.plotstyle = 'b-'
-    
+        
     #
     #  Gauge Location Plot
     #
@@ -221,6 +232,9 @@ def setplot(plotdata=None):
         gaugetools.plot_gauge_locations(cd.plotdata, gaugenos='all',
                                         format_string='ko', add_labels=True)
 
+        
+    #gauge_num_list = np.arange(1,5,1)
+
     plotfigure = plotdata.new_plotfigure(name="Gauge Locations")
     plotfigure.show = True
 
@@ -228,22 +242,27 @@ def setplot(plotdata=None):
     plotaxes = plotfigure.new_plotaxes()
     plotaxes.title = 'Gauge Locations'
     plotaxes.scaled = True
-    plotaxes.xlimits = [-88.6, -87.1]
-    plotaxes.ylimits = [30.20, 30.75]
+    plotaxes.xlimits = [-88.75, -87]
+    plotaxes.ylimits = [30.1, 30.82]
     plotaxes.afteraxes = gauge_location_afteraxes
     surgeplot.add_surface_elevation(plotaxes, bounds=surface_limits)
     surgeplot.add_land(plotaxes, bounds=[0.0, 20.0])
     plotaxes.plotitem_dict['surface'].amr_patchedges_show = [0] * 10
     plotaxes.plotitem_dict['land'].amr_patchedges_show = [0] * 10
 
+    
+
+    ###############################################################################
+    ###############################################################################
     # -----------------------------------------
     # Parameters used only when creating html and/or latex hardcopy
     # e.g., via pyclaw.plotters.frametools.printframes:
+    
 
     plotdata.printfigs = True                # print figures
     plotdata.print_format = 'png'            # file format
     plotdata.print_framenos = 'all'          # list of frames to print
-    plotdata.print_gaugenos = 'all'   # list of gauges to print
+    plotdata.print_gaugenos = 'all'          # list of gauges to print
     plotdata.print_fignos = 'all'            # list of figures to print
     plotdata.html = True                     # create html files of plots?
     plotdata.latex = True                    # create latex file of plots?
@@ -252,5 +271,4 @@ def setplot(plotdata=None):
     plotdata.latex_makepdf = False           # also run pdflatex?
     plotdata.parallel = True                 # parallel plotting
 
-    print(type(plotdata))
     return plotdata
